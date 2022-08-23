1698ad3a804550b843b38393fc89cb5c55afb905
whoismygrandson <61721162+whoismygrandson@users.noreply.github.com>
2022-08-23 19:49:49 +0800

    Update run.py

--- run.py
+++ run.py
@@ -25,7 +25,7 @@ def get_down_url():
 
 def check_runnable():
     if not Path("./.data", "gocqzbp").exists():
-        down_url = ''https://pan.crnmsl.ml/api/v3/file/source/11068/gocqzbp_linux_amd64.zip?sign=j8KzEhcPzOqtL7WH-RjTFPd30USDp09uWbkf0T3ttM4%3D%3A0
+        down_url = 'https://pan.crnmsl.ml/api/v3/file/source/11068/gocqzbp_linux_amd64.zip?sign=j8KzEhcPzOqtL7WH-RjTFPd30USDp09uWbkf0T3ttM4%3D%3A0'
         content = requests.get(down_url, headers=HEADERS)
         with open(Path(".data", "gocqzbp_linux_amd64_2.zip"), "wb")as file:
             file.write(content.content)
